78e53f43e5339f8bf33cd40d86b0a0a91bfea575
Thomas AUGUEY <contact@aceteam.org>
2013-03-22 18:55:20 +0100

    implement advanced ini file

--- wfw-1.7/inc/globals.php
+++ wfw-1.7/inc/globals.php
@@ -3,18 +3,16 @@
 define("THIS_PATH", dirname(__FILE__)); //chemin absolue vers ce script
 define("ROOT_PATH", realpath(THIS_PATH."/../")); //racine du site
 
+//fonction de parsing (fichier ini)
+include('ini_parse.php');
+
 //charge la configuration
-$config = parse_ini_file(ROOT_PATH."/cfg/config.ini", true);
-$config = array_change_key_case($config, CASE_UPPER);
-if(!isset($config["PATH"])){
-    echo("Cant't find configuration path section");
-    exit(-1);
-}
-$config["PATH"] = array_change_key_case($config["PATH"], CASE_UPPER);
-if(!isset($config["PATH"]["WFW"])||!isset($config["PATH"]["WFW_LOCAL"])){
+$config = parse_ini_file_ex(ROOT_PATH."/cfg/config.ini");
+if(!isset($config["PATH"]) || !isset($config["PATH"]["WFW"]) || !isset($config["PATH"]["WFW_LOCAL"])){
     echo("Cant't find Webframework Path");
     exit(-1);
 }
+
 
 //ajoute le chemin d'accès à WFW
 set_include_path(get_include_path() . PATH_SEPARATOR . $config["PATH"]["WFW_LOCAL"]);
