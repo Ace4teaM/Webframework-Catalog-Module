e8ed96774234b150b8172f833965680970904892
Thomas AUGUEY <contact@aceteam.org>
2014-03-11 18:57:22 +0100

    implement SQL sequences

---
+++ sql/postgresql/objects.sql
@@ -0,0 +1,14 @@
+/*
+  (C)2014 Thomas AUGUEY
+  PL/pgSQL
+  Module Catalog
+  
+  Index, Sequences et autres objets
+*/
+
+/*
+    Sequences
+    Liste des sequences d'auto incrementation pour les identifiants 
+*/
+DROP SEQUENCE IF EXISTS catalog_item_seq;
+CREATE SEQUENCE catalog_item_seq START 1;
